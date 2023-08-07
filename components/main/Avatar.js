c6d77fc406bc41da2dd73ec817b3699d79a08c46
Sonny C <sonny.capisciolti@gmail.com>
2023-08-07 14:57:55 +0200

    pblm image face

--- components/main/Avatar.js
+++ components/main/Avatar.js
@@ -1,10 +1,13 @@
 import React from "react";
+// import face from "./avatar/face.png";
 
 export default function Avatar() {
   return (
     <div className="justify-end my-auto w-fit md:w-full md:flex">
       <img
-        src="https://media.discordapp.net/attachments/720905773256933457/1137052159582675014/IMG_0838__1_-removebg-preview_2-modified.png"
+        src={
+          "https://cdn.discordapp.com/attachments/720905773256933457/1137052159582675014/IMG_0838__1_-removebg-preview_2-modified.png"
+        }
         className="rounded-full shadow-lg w-52 h-fit md:w-60"
         alt="Avatar"
       />
